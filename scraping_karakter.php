3ddb28726ff63a98048b29c925cb6aba80bfd609
Rendyfranzz <90760961+Rendyfranzz@users.noreply.github.com>
2021-09-18 20:27:17 +0700

    Add files via upload

---
+++ scraping_karakter.php
@@ -0,0 +1,20 @@
+<?php
+include 'config.php';
+include 'simple_html_dom.php';
+
+$karakter="https://www.gensh.in/characters";
+$html =file_get_html($karakter);
+$variable=$html->find('div[class=row character-list]',0);
+
+for($i=0; $i < 42; $i++)
+{
+
+        $link=$variable->children($i)->children(0)->children(0)->children(0)->children(0)->src;
+        $name=$variable->children($i)->children(0)->children(1)->children(0)->plaintext;
+        $type=$variable->children($i)->children(0)->children(1)->children(1)->plaintext;
+        mysqli_query($db_connection,"INSERT INTO karakter (link_img,nama_karakter,tipe_karakter) VALUES ('https://www.gensh.in$link','$name','$type')");
+        
+    
+}
+  
+?>
